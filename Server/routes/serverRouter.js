6f12218546bdf9e68bb737e5bee25ef7c5d3ef8d
kc-code32 <kelvinchen138@gmail.com>
2024-03-06 15:01:58 -0600

    refactored router

--- Server/routes/serverRouter.js
+++ Server/routes/serverRouter.js
@@ -17,7 +17,6 @@ router.post(
 
 router.post(
   '/register',
-  // sessionController.deleteSession,
   userController.createUser,
   cookieController.setSSIDCookie,
   sessionController.startSession,
@@ -31,10 +30,9 @@ router.post(
 
 router.post(
   '/login',
-  // sessionController.deleteSession,
   userController.verifyUser,
-  // cookieController.setSSIDCookie,
-  // sessionController.startSession,
+  cookieController.setSSIDCookie,
+  sessionController.startSession,
   (req, res) => {
     return res.status(200).json({
       loggedIn: res.locals.signedIn,
@@ -44,34 +42,32 @@ router.post(
 );
 
 router.patch(
-  //NEED ROUTE from Front End
-  //KELVIN - Should we update below to '/favoriteAdd' ?
   '/favoriteAdd',
   userController.favorites,
   (req, res) => {
     return res.status(200).json({
-      //NEED Return info from Front End
       addedFavoritesList: res.locals.addedFavoriteList,
     });
   }
 );
 
 router.patch(
-  //NEED ROUTE from Front End
   '/favoriteDelete',
   userController.deleteFavorites,
   (req, res) => {
     return res.status(200).json({
-      //NEED return info from Front End
       deletedFavoritesList: res.locals.deletedFavoritesList,
     });
   }
 );
 
-router.get('/isLoggedIn', sessionController.isLoggedIn, (req, res) => {
-  return res.json({
-    loggedIn: res.locals.signedIn,
-    id: req.cookies.ssid,
+router.get(
+  '/isLoggedIn', 
+  sessionController.isLoggedIn, 
+  (req, res) => {
+    return res.json({
+      loggedIn: res.locals.signedIn,
+      id: req.cookies.ssid,
   });
 });
 
@@ -79,7 +75,7 @@ router.get(
   '/logout', 
   sessionController.clearSession,
   (req, res) => {
-    return res.status(200).clearCookie('cookieId').redirect('/homepage');
+    return res.status(200).clearCookie('ssid').redirect('/homepage');
   }
 );
 
